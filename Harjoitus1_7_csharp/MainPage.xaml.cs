004e9bca67b98992e5f1e27d2b2952bdcd2a2359
Elmo-H <elmo.huoku@gmail.com>
2022-04-03 14:17:06 +0300

    Exercise finished.

---
+++ Harjoitus1_7_csharp/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Harjoitus1_7_csharp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            button1_Click(sender, e, box);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e, Border box)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 20);
+
+            if (num <= 9)
+            {
+                SolidColorBrush mySolidColorBrush = new SolidColorBrush(Windows.UI.Colors.Blue);
+                box.Background = mySolidColorBrush;
+            }
+
+            if (num >= 11)
+            {
+                SolidColorBrush mySolidColorBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+                box.Background = mySolidColorBrush;
+            }
+
+            if(num == 10)
+            {
+                SolidColorBrush mySolidColorBrush = new SolidColorBrush(Windows.UI.Colors.Yellow);
+                box.Background = mySolidColorBrush;
+            }
+            textBlock1.Text = num.ToString();
+        }
+    }
+}
